4bf70fa859fc8d14aaeaa337450d0dddd76c4395
giovanni <giovanni.maia@gmail.com>
2020-03-21 10:49:43 +0100

    initial commit

---
+++ src/main/kotlin/com/colivery/engine/EngineApplication.kt
@@ -0,0 +1,11 @@
+package com.colivery.engine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<EngineApplication>(*args)
+}
