ec406678200917605215dec15a273f63f9371c35
Stefan Hoferer <s.hoferer@hse24.de>
2020-03-22 10:44:21 +0100

    add simple startup test

--- src/test/kotlin/com/colivery/engine/EngineApplicationTests.kt
+++ src/test/kotlin/com/colivery/engine/EngineApplicationTests.kt
@@ -2,11 +2,15 @@ package com.colivery.engine
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@Import(TestConfig::class)
+@ActiveProfiles("test")
 class EngineApplicationTests {
 
-	//@Test
+	@Test
 	fun contextLoads() {
 	}
 
