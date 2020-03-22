387cbdb9065efb13d1640cccd8bd1c54d31df7f3
giovanni <giovanni.maia@gmail.com>
2020-03-22 11:04:54 +0100

    removing debugging code..

--- src/main/kotlin/com/colivery/engine/EngineController.kt
+++ src/main/kotlin/com/colivery/engine/EngineController.kt
@@ -7,7 +7,6 @@ import com.colivery.engine.service.poi.PoiSearchService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -24,12 +23,6 @@ class EngineController {
     @Autowired
     lateinit var poiSearchService: PoiSearchService
 
-    @GetMapping("/")
-    fun get(): Array<PoI> {
-        logger.info("GET /")
-        return poiSearchService.findPoIs(Coordinate(48.16058943132621, 11.565932035446167), 2.0F)
-    }
-
     @Autowired
     lateinit var distanceService: DistanceService
 
@@ -38,7 +31,7 @@ class EngineController {
 
     @PostMapping("/search")
     fun search(@RequestBody @Valid request: SearchRequest): SearchResponse? {
-        logger.info("GET /search $request")
+        logger.info("POST /search $request")
 
         val startLocation = request.coordinate
         val radius = request.range
