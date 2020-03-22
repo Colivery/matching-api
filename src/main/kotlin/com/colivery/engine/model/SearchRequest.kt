ae932167b2088b281e47abde19fbaf2a1143d9e9
giovanni <giovanni.maia@gmail.com>
2020-03-22 11:02:49 +0100

    trying to make "cascaded validations" work...

--- src/main/kotlin/com/colivery/engine/model/SearchRequest.kt
+++ src/main/kotlin/com/colivery/engine/model/SearchRequest.kt
@@ -1,10 +1,16 @@
 package com.colivery.engine.model
 
+import org.springframework.validation.annotation.Validated
+import javax.validation.Valid
 import javax.validation.constraints.Max
 import javax.validation.constraints.Min
 
-data class SearchRequest(val coordinate: Coordinate) {
-    @Min(0)
-    @Max(10)
-    val range: Float = 5.0F
-}
+@Validated
+data class SearchRequest(
+        @Valid
+        val coordinate: Coordinate,
+
+        @Min(0)
+        @Max(10)
+        val range: Float = 5.0F
+)
